efd2b071a1bd10108907041ccab62ef0620439d1
Pawel <wydrapawel@icloud.com>
2022-08-11 08:25:13 +0200

    add database, conneted with index list element

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ from flask import Flask, jsonify, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_ckeditor import CKEditor, CKEditorField
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
-import random
 
-#
 class NewTask(FlaskForm):
-    name = StringField("Name", validators=[DataRequired()])
-    task_description = StringField("Description",validators=[DataRequired()])
-    submit = SubmitField("Add Cafe")
+    name = StringField("Name", validators=[DataRequired()], render_kw={"placeholder": "Task Name"})
+    task_description = CKEditorField("Description", validators=[DataRequired()])
+    submit = SubmitField("Add Task")
 
 
 app = Flask(__name__)
 Bootstrap(app)
+ckeditor = CKEditor(app)
 
 # Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Task.db'
@@ -23,29 +23,37 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-# Cafe TABLE Configuration
+# Task TABLE Configuration
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    description= db.Column(db.String(250), unique=True, nullable=False)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    description = db.Column(db.String(250), unique=True, nullable=False)
 
 
 # def to_dict(self):
 #     return {column.name: getattr(self, column.name) for column in self.__table__.columns}
-
+# db.create_all()
 
 @app.route("/")
 def home():
     # todos = Cafe.query.filter_by(location=query_location).all()
-    # done = Cafe.query.filter_by(location=query_location).all()
-
-    return render_template("index.html")
+    all_tasks = Task.query.all()
+    db.session.commit()
+    return render_template("index.html", all_tasks=all_tasks)
 
 
 @app.route("/new-task", methods=["POST", "GET"])
 def new_task():
     form = NewTask(meta={'csrf': False})
     if form.validate_on_submit():
+        add_task = Task(
+            name=request.form.get("name"),
+            description=request.form.get("task_description")
+           )
+        db.session.add(add_task)
+        db.session.commit()
         return redirect(url_for("home"))
+
     return render_template("new_task.html", form=form)
 
 
@@ -113,33 +121,6 @@ def new_task():
 #     else:
 #         return jsonify(error={"Not Found": "Sorry, that's not allowed. Make sure you have the correct api_key."}), 404
 
-#
-# @app.route("/selected_coffee/<int:cafe_id>", methods=["POST", "GET"])
-# def selected_coffee(cafe_id):
-#     cafe = db.session.query(Cafe).get(cafe_id)
-#     return render_template("cafe.html", cafe=cafe)
-
-
-# @app.route("/new_cafe", methods=["POST", "GET"])
-# def new_cafe():
-#     form = NewCafe(meta={'csrf': False})
-#     if form.validate_on_submit():
-#         new_cafe_add = Cafe(
-#             name=request.form.get("name"),
-#             map_url=request.form.get("map_url"),
-#             img_url=request.form.get("img_url"),
-#             location=request.form.get("location"),
-#             has_sockets=bool(request.form.get("sockets")),
-#             has_toilet=bool(request.form.get("toilet")),
-#             has_wifi=bool(request.form.get("wifi")),
-#             can_take_calls=bool(request.form.get("calls")),
-#             seats=request.form.get("seats"),
-#             coffee_price=request.form.get("coffee_price"),
-#         )
-#         db.session.add(new_cafe_add)
-#         db.session.commit()
-#         return redirect(url_for("home"))
-#     return render_template("new_cafe.html", form=form)
 
 
 if __name__ == '__main__':
